a8a81140fb1ecdd097e62ae02eba8a4dd1ae0820
Artem Sokolov <Artemur18@gmail.com>
2016-07-22 18:47:22 +0300

    Added basic text counter

--- js/run.js
+++ js/run.js
@@ -1,1 +1,21 @@
-console.log('tap tap tap');
+var worsArray = [];
+$(document).ready(function() {
+
+  $("#textMainArea").on('keyup', function() {
+
+  	wordsArray = this.value.match(/\S+/g);
+
+  	console.log();
+
+    if(wordsArray){
+
+   		var wordsLength = wordsArray.length;
+   		$('.wordCount').text(wordsLength);
+
+  	}
+
+  	else $('.wordCount').text('0');
+
+  });
+ 
+}); 
